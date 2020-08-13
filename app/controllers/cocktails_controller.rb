507fe2235d467ad710fc5dbf5896d17fa44a5e91
josephlarue <joseph.larue@edhec.com>
2020-08-13 15:01:03 +0200

    exo

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,28 @@
+class CocktailsController < ApplicationController
+    def new
+        @cocktail = Cocktail.new
+    end
+
+    def create
+        @cocktail = Cocktail.new(params_cocktail)
+        if @cocktail.save
+          redirect_to cocktail_path(@cocktail)
+        else
+          render :new
+        end
+    end
+
+    def show
+        @cocktail = Cocktail.find(params[:id])
+    end
+
+    def index
+        @cocktails = Cocktail.all
+    end
+
+    private 
+
+    def params_cocktail
+        params.require(:cocktail).permit(:name)
+    end
+end
---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,11 @@
+class IngredientsController < ApplicationController
+
+    def show
+        @ingredient = Ingredient.find(params[:id])
+    end
+
+    def index
+        @ingredients = Ingredient.all
+    end
+
+end
